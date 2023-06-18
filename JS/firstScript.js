72f10d3fd991cac384612e782573275af25442d6
Ethannn_boi <84193841+B4ethan@users.noreply.github.com>
2023-06-18 07:39:01 +0000

    fun fun with buutons and changing strings

--- JS/firstScript.js
+++ JS/firstScript.js
@@ -1,1 +1,12 @@
-alert("Hello world");
+let s1;
+let s2;
+let s3;
+
+
+document.getElementById("resultBtn").onclick = function(){
+    s1 = document.getElementById("first").value;
+    s2 = document.getElementById("second").value;
+    s3 = document.getElementById("third").value; 
+
+    document.getElementById("resultText").innerHTML = s1 + " " + s2 + " " +s3
+}
